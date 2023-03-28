284422a0da620895a24d3fd71511510c45163d97
aditya pawar <99139758+aditya132p@users.noreply.github.com>
2023-03-28 08:56:47 -0700

    add website files
    project section is in complete need to be change.

---
+++ Portfolio-Website/index.js
@@ -0,0 +1,24 @@
+//  main text typed animation
+var typed = new Typed(".multiple-text", {
+  strings: ["", "Frontend Developer", "Backend Developer.", "Blogger"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+//  menu toggle
+
+//  var menuIcons = document.querySelector(".fa-solid").onclick =function (){
+//   var navbar = document.querySelector(".nav-items");
+//    menuIcons.classList.toggle("fa-xmark");
+//   navbar.classList.toggle("active");
+
+//   };
+
+// // home section resume button
+// function gotoLink(link){
+//     console.log(link.value);
+//     window.open(link.value);
+// }
+
+// //scroll efect
