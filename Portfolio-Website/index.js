a01fe5e5d26e927286ffd24bfe6ec07f126a41f4
aditya pawar <99139758+aditya132p@users.noreply.github.com>
2023-06-26 00:47:37 +0530

    Update index.js
    update javascript

--- Portfolio-Website/index.js
+++ Portfolio-Website/index.js
@@ -8,17 +8,70 @@ var typed = new Typed(".multiple-text", {
 });
 //  menu toggle
 
-//  var menuIcons = document.querySelector(".fa-solid").onclick =function (){
-//   var navbar = document.querySelector(".nav-items");
-//    menuIcons.classList.toggle("fa-xmark");
-//   navbar.classList.toggle("active");
+document.getElementById("ham").onclick = function () {
+ 
+  navBar = document.querySelector(".nav-items");
+  navBar.classList.toggle("active");
 
-//   };
+  icon = document.querySelector("#ham");
+  icon.classList.toggle("bx-x");
+  icon.classList.remove("bx-menu");
+  icon.classList.toggle("bx-menu");
+};
 
-// // home section resume button
-// function gotoLink(link){
-//     console.log(link.value);
-//     window.open(link.value);
-// }
+document.querySelector(".nav-items").onclick = function () {
+  navBar = document.querySelector(".nav-items");
+  navR = document.querySelector("#ham");
+  navBar.classList.remove("active");
+  navR.classList.remove("bx-x");
 
-// //scroll efect
+  //    x.classList.remove("bx-x");
+};
+
+
+//  home section resume button
+ function gotoLink(link){
+     console.log(link.value);
+     window.open(link.value);
+ }
+
+// scroll effect animations
+window.addEventListener("scroll", Reveal);
+
+function Reveal(){
+  var RevealAnimation = document.querySelectorAll(".Reveal");
+
+  for (var i=0; i < RevealAnimation.length; i++) {
+    var windoHeight = window.innerHeight;
+    var revealTop = RevealAnimation[i].getBoundingClientRect().top;
+    var revealPoint = 150;
+
+    if (revealTop < windoHeight - revealPoint) {
+      RevealAnimation[i].classList.add("animation-top");
+    }
+    else{
+      RevealAnimation[i].classList.remove("animation-top");
+    }
+    
+  }
+}
+
+// scroll active link
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+ 
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+ 
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active-link');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active-link');
+            });
+        };
+    });
+};
